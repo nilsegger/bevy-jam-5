1e27549d5651c9cd338eedbd1a621618dd3b1c6d
nilsegger <nilsxegger@gmail.com>
2024-07-23 14:26:40 +0200

    added todo for fullscreen

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,32 @@ mod building;
 mod layers;
 
 use avian2d::{debug_render::PhysicsDebugPlugin, PhysicsPlugins};
-use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
+use bevy::{asset::AssetMetaCheck, window::WindowResolution};
 
 use crate::building::BuildingsPlugin;
+
+// TODO: detect window resizes for fullscreen support
 
 /// Main
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // Wasm builds will check for meta files (that don't exist) if this isn't set.
-            // This causes errors and even panics in web builds on itch.
-            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-            meta_check: AssetMetaCheck::Never,
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins.set(AssetPlugin {
+                // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                // This causes errors and even panics in web builds on itch.
+                // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                meta_check: AssetMetaCheck::Never,
+                ..default()
+            }), // .set(WindowPlugin {
+                //     primary_window: Some(Window {
+                //         resolution: WindowResolution::new(640., 360.)
+                //             .with_scale_factor_override(1.0),
+                //         ..default()
+                //     }),
+                //     ..default()
+                // }),
+        )
         .add_plugins((BuildingsPlugin,))
         .add_plugins((PhysicsPlugins::default(), PhysicsDebugPlugin::default()))
         .add_systems(Startup, setup_camera)
@@ -51,7 +62,7 @@ pub fn close_on_esc(
 /// Init 2d camera
 fn setup_camera(mut commands: Commands) {
     commands.spawn((Camera2dBundle {
-        transform: Transform::from_xyz(0.0, 200.0, 0.0),
+        transform: Transform::from_xyz(0.0, 100.0, 0.0),
         ..default()
     },));
 }
